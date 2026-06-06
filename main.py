25da0aa928dd13bd7d54456bc81585d69b0705d9
Dmytro Vitiuk <yo@MacBook-Air.local>
2026-06-06 18:38:38 +0300

    create needed files

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+import settings
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 800
+BACKGROUND_COLOR = settings.BACKGROUND_COLOR
+DOT_COLOR = settings.DOT_COLOR
+SHAPE_COLOR = settings.SHAPE_COLOR
+LINE_WIDTH = settings.LINE_WIDTH
+SCALE = settings.SCALE
+FPS = settings.FPS
+
+
